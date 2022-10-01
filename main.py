8a9d6932161406b87a14535ead903ae05ad10711
Mahlet Edeti <mwedeti@cougarnet.uh.edu>
2022-10-01 17:50:05 -0500

    This is my second attempt

--- main.py
+++ main.py
@@ -1,47 +1,12 @@
 import mysql.connector
 from mysql.connector import Error
 import flask
+from sql import create_connection
+from sql import execute_query
+from sql import execute_read_query
 from flask import jsonify
 from flask import request
 import creds
-
-
-def create_connection(host_name, user_name, user_password, db_name):
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password,
-            database=db_name
-        )
-        print("Connection to MySQL DB successful")
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
-    return connection
-
-
-def execute_query(connection, query):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        connection.commit()
-        print("Query executed successfully")
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
-
-def execute_read_query(connection, query):
-    cursor = connection.cursor(dictionary=True)
-    result = None
-    try:
-        cursor.execute(query)
-        result = cursor.fetchall()
-        return result
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
 
 app = flask.Flask(__name__)  # sets up the application
 app.config["DEBUG"] = True  # allow to show errors in browser
@@ -93,7 +58,4 @@ def delete_gem():
 
 
 app.run()
-
-
-
 

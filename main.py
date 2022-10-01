d6c8a59b4e35a96f4a4dda94f508a61640965174
Mahlet Edeti <mwedeti@cougarnet.uh.edu>
2022-10-01 18:16:31 -0500

    This is my second attempt

--- main.py
+++ main.py
@@ -1,27 +1,58 @@
 import mysql.connector
 from mysql.connector import Error
 import flask
-from sql import create_connection
-from sql import execute_query
-from sql import execute_read_query
 from flask import jsonify
 from flask import request
 import creds
 
-app = flask.Flask(__name__)  # sets up the application
-app.config["DEBUG"] = True  # allow to show errors in browser
+#creating a connection
+def create_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+def execute_read_query(connection, query):
+    cursor = connection.cursor(dictionary=True)
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+app = flask.Flask(__name__) #sets up the application
+app.config["DEBUG"] = True #allow to show errors in browser
 
 myCreds = creds.Creds()
 conn = create_connection(myCreds.constring, myCreds.username, myCreds.password, myCreds.dbName)
 
-
+# getting all the data using GET
 @app.route('/api/gem', methods=['GET'])
 def get_gem():
     sql = "SELECT * FROM gem"
     users = execute_read_query(conn, sql)
     return users
-
-
+#posting the data
 @app.route('/api/gem', methods=['POST'])
 def post_gem():
     request_data = request.get_json()
@@ -29,12 +60,10 @@ def post_gem():
     newcolor = request_data['gemcolor']
     newcarat = request_data['carat']
     newprice = request_data['price']
-    sql = "INSERT INTO gem(gemtype, gemcolor, carat, price) VALUES ('%s','%s',%s,%s)" % (
-    newtype, newcolor, newcarat, newprice)
+    sql = "INSERT INTO gem(gemtype, gemcolor, carat, price) VALUES ('%s','%s',%s,%s)" % (newtype, newcolor, newcarat, newprice)
     execute_query(conn, sql)
     return "added sucessfully"
-
-
+#updating the data using API PUT
 @app.route('/api/gem', methods=['PUT'])
 def put_gem():
     id = int(request.args['id'])
@@ -43,19 +72,18 @@ def put_gem():
     newcolor = request_data['gemcolor']
     newcarat = request_data['carat']
     newprice = request_data['price']
-    sql = "UPDATE gem SET gemtype = '%s', gemcolor = '%s', carat = %s, price = %s WHERE id = %s" % (
-    newtype, newcolor, newcarat, newprice, id)
-    execute_query(conn, sql)
+    sql = "UPDATE gem SET gemtype = '%s', gemcolor = '%s', carat = %s, price = %s WHERE id = %s" % (newtype, newcolor, newcarat, newprice, id)
+    execute_query(conn,sql)
     return "updated sucessfully"
-
-
+#Deleting the data using API DELETE
 @app.route('/api/gem', methods=['DELETE'])
 def delete_gem():
     id = int(request.args['id'])
     sql = "DELETE FROM gem WHERE id = %s" % (id)
-    execute_query(conn, sql)
+    execute_query(conn,sql)
     return "Delete request successful"
 
 
 app.run()
+
 
